d7a95e257637a05d27fa8e5cdc0ee12b15d6df17
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-20 15:59:36 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,7 @@ const errors = require("../utils/errors.js");
 module.exports.run = async (bot, message, args) => {
 
   if(message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MEMBERS");
-  if(args[0]) return errors.noPerms(message, "Contacta con un Administrador");
+   if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`${args[0]} mensajes eliminados correctamente.`).then(m => m.delete(5000));
 });
