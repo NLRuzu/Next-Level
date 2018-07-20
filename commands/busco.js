3aaaf7e837f16b43b9647d077be8af2347479084
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-20 14:33:19 +0200

    Create busco.js

---
+++ commands/busco.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+      let cantidad = args[0];
+      let juego = args[1];
+      let sicon = message.guild.iconURL;
+      let reportEmbed = new Discord.RichEmbed()
+      .setTitle(message.member.nickname)
+      .setDescription("Busca **"+ cantidad+"** personas para jugar "+juego)
+      .setColor("#8904B1")
+      .addField("**En la sala**", message.member.voiceChannel);
+
+      let partidaschannel = message.guild.channels.find(`name`, "ðŸ’Žbuscar-partidas");
+      if(!partidaschannel) return message.channel.send("Introduce bien el comando");
+
+
+      message.delete().catch(O_o=>{});
+      partidaschannel.send(reportEmbed);
+
+
+}
+
+module.exports.help = {
+  name: "busco"
+}
