f625fcf12337e7706bb25ae7dfd804bc92df1a7e
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-20 16:02:15 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,11 +3,11 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MEMBERS");
-   if(!args[0]) return message.channel.send("oof");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`${args[0]} mensajes eliminados correctamente.`).then(m => m.delete(5000));
-});
+    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+  });
 }
 
 module.exports.help = {
