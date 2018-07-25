97db06561e128edb036aca49fe7ff67f4f1071b7
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-25 20:38:56 +0200

    Add files via upload

---
+++ commands/report.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("Couldn't find user.");
+    let rreason = args.join(" ").slice(22);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Informe de Toques")
+    .setColor("#15f153")
+    .addField("Usuario reportado:", `${rUser}`)
+    .addField("Reportado por:", `${message.author}`)
+    .addField("Fecha:", message.createdAt)
+    .addField("Razón:", rreason);
+
+    let reportschannel = message.guild.channels.find(`name`, "❗-toques");
+    if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+
+
+    message.delete().catch(O_o=>{});
+    reportschannel.send(reportEmbed);
+
+}
+
+module.exports.help = {
+  name: "toque"
+}
