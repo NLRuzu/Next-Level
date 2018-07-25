76a008fde287a0215786200f9de30528085e4824
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-25 22:51:28 +0200

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("Couldn't find user.");
+    if(!rUser) return message.channel.send("formato incorrecto +report @usuario razón");
     let rreason = args.join(" ").slice(22);
 
     let reportEmbed = new Discord.RichEmbed()
