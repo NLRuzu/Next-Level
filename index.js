97386661943fcfa35dcb12f5a26a9778b3b15bfa
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 22:50:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ bot.on("message", (message) => {
   if(message.content.toUpperCase().startsWith("+CSGO")){
         message.delete();
             if(message.member.roles.find("name", "✅Verificado")){
+                let User = message.member;
                 let role = message.guild.roles.find("name", "💣 CSGO");
                 let guild = bot.guilds.get("458220475957379074");
                 let miembro = guild.member(User);
