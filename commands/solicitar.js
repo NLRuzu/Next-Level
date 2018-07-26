ee576eceb3671909ade8787a90351af7d761c2a5
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 12:06:55 +0200

    Update solicitar.js

--- commands/solicitar.js
+++ commands/solicitar.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
       let sicon = message.guild.iconURL;
       let reportEmbed = new Discord.RichEmbed()
       .setTitle(message.member.nickname)
-      .setDescription("**Está solicitando**"+ cantidad+"** para ver todo el contenido**")
+      .setDescription("**Está solicitando **"+ cantidad+"** para ver todo el contenido**")
       .setColor("#8904B1")
       .addField("**En la sala**", message.member.voiceChannel);
 
