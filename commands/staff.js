135cd34f14402ed482a92b46ffb2678b311ffd80
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 12:17:14 +0200

    Add files via upload

---
+++ commands/staff.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("**COMANDOS STAFF**")
+    .setColor("#FE2E2E")
+    .setThumbnail(sicon)
+    .addField("**+report @user Razón**", "Damos un toque a un usuario")
+    .addField("**+verificado @User**", "Tras asignarle el rol de Verificado, ponemos el mensaje en #verificados")
+
+
+    return message.channel.send(serverembed);
+}
+
+module.exports.help = {
+  name: "staff"
+}
