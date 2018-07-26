5ad60dfddffe97562c8da3fd80759be0e18676a2
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 22:56:24 +0200

    Create roles.js

---
+++ commands/roles.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("**GENERALES**")
+    .setColor("#FE2E2E")
+    .setThumbnail(sicon)
+    .addField("**+rollcsgo**", "Te añade el rol de CSGO")
+    .addField("**+rollfortnite**", "Te añade el rol de FORTNITE");
+    
+   
+
+
+    return message.channel.send(serverembed);
+}
+
+module.exports.help = {
+  name: "roles"
+}
