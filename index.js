cf0166b1d39c79152b0452a23e82de683220d8b8
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-27 01:50:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,123 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const token = process.env.token;
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() == "js")
+  if(jsfile.length <= 0){
+    console.log("No se encuentra el comando");
+    return
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} cargado`);
+    bot.commands.set(props.help.name, props);
+  });
+
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} estÃ¡ online`);
+  bot.user.setActivity("+help")
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.token);
+bot.on("guildMemberAdd", async member => {
+  console.log(`${member.id} ha entrado al server `);
+
+  let welcomechannel = member.guild.channels.find(`name`, "ðŸ“‘-entrada-y-salida");
+  welcomechannel.send(`Â¡Bienvenido ${member} a  Next Level Clan** :tada::hugging: !`);
+});
+
+bot.on("guildMemberRemove", async member => {
+  console.log(`${member.id} ha abandonado el server `);
+
+  let welcomechannel = member.guild.channels.find(`name`, "ðŸ“‘-entrada-y-salida");
+  welcomechannel.send(`${member} ha abandonado el clan:slight_frown:`);
+});
+
+bot.on("guildMemberAdd", function(member) {
+    let role = member.guild.roles.find("name", "âŽ No verificado");
+    member.addRole(role).catch(console.error);
+});
+
+
+bot.on("message", (message) => {
+  if(message.content.toUpperCase().startsWith("+VERIFICAR")){
+        message.delete();
+            if(message.member.roles.find("name", "ðŸŒŸ STAFF NIVEL 3") || message.member.roles.find("name", "ðŸŒŸ STAFF NIVEL 2") ||message.member.roles.find("name", "ðŸŒŸ STAFF NIVEL 1")){
+                let User = message.mentions.users.first();
+                let role = message.guild.roles.find("name", "âœ…Verificado");
+                let role2 = message.guild.roles.find("name", "âŽ No verificado");
+                let guild = bot.guilds.get("458220475957379074");
+                let miembro = guild.member(User);
+                miembro.addRole(role).catch(console.error);
+                miembro.removeRole(role2).catch(console.error);
+                message.channel.send({
+                    embed: {
+                        color: 0x04ff00,
+                        title: message.member.nickname,
+                        description: "**Ha verificado a **" + User + " **correctamente**",
+                    }
+                });
+            }
+        }
+  console.log("True");
+  
+  if(message.content.toUpperCase().startsWith("+ROLLCSGO")){
+        message.delete();
+            if(message.member.roles.find("name", "âœ…Verificado")){
+                let User = message.member;
+                let role = message.guild.roles.find("name", "ðŸ’£ CSGO");
+                let guild = bot.guilds.get("458220475957379074");
+                let miembro = guild.member(User);
+                miembro.addRole(role).catch(console.error);
+                message.channel.send({
+                    embed: {
+                        color: 0x04ff00, 
+                        description: "**ROL de CSGO asignado a **" + User + " **correctamente**",
+                    }
+                });
+            }
+        }
+  console.log("True");
+  
+  if(message.content.toUpperCase().startsWith("+ROLLFORTNITE")){
+        message.delete();
+            if(message.member.roles.find("name", "âœ…Verificado")){
+                let User = message.member;
+                let role = message.guild.roles.find("name", "ðŸ¤– Fortnite");
+                let guild = bot.guilds.get("458220475957379074");
+                let miembro = guild.member(User);
+                miembro.addRole(role).catch(console.error);
+                message.channel.send({
+                    embed: {
+                        color: 0x04ff00, 
+                        description: "**ROL de FORTNITE asignado a **" + User + " **correctamente**",
+                    }
+                });
+            }
+        }
+  console.log("True");
+ 
+  
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+  
+
+});
+
+bot.login(token).catch(err => console.log(err));
