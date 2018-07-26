1a310cf27e96603218ac11d1aa87a62b1d4c714f
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 23:35:32 +0200

    Update proponer.js

--- commands/proponer.js
+++ commands/proponer.js
@@ -19,9 +19,6 @@ module.exports.run = async (bot, message, args) => {
     
     message.delete().catch(O_o=>{});
     reportschannel.send(reportEmbed);
-    .then((newMessage) => {
-        newMessage.react("✔️")
-        })
 
 }
 
