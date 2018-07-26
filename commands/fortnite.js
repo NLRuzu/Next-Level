5e1cddaa675ee4aec2505b8be6b2db08b196654c
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 22:58:59 +0200

    Update fortnite.js

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -3,13 +3,14 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
-    .setDescription("**FORTNITE**")
+    .setTitle("**FORTNITE**")
     .setColor("#FE2E2E")
     .setThumbnail(sicon)
     .addField("**.ign plataforma nickname**", "Vincula tu cuenta de epicgames.")
     .addField("**.wins**", "Asigna las wins a tu nick, hay que actualizar constantemente.")
     .addField("**.kd**", "Asigna tu kd a tu nick, hay que actualizar constantemente.")
     .addField("**.stats**", "Te muestra tus stats generales.")
+    .addField("**.season5**", "Te muestra tus stats de la temporada 5.");
 
 
     return message.channel.send(serverembed);
