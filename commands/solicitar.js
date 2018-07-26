67c360810c62601f321a6ab53b376f3ae7f4f61f
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 21:19:19 +0200

    Update solicitar.js

--- commands/solicitar.js
+++ commands/solicitar.js
@@ -1,16 +1,16 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("formato incorrecto +solicitar @usuario razón");
-    let rreason = args.join(" ").slice(22);
+    let rUser = message.guild.member(message.mentions.users.first());
+    if(!rUser) return message.channel.send("formato incorrecto +solicitar @usuario");
+    
 
     let reportEmbed = new Discord.RichEmbed()
     .setDescription("Nueva petición de acceso")
     .setColor("#15f153")
     .addField("Solicitante:", `${message.author}`)
     .addField("Fecha:", message.createdAt)
-    .addField("Razón:", rreason);
+    
 
     let reportschannel = message.guild.channels.find(`name`, "❎-solicitudes");
     if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
