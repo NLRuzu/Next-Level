06dea26494c9898e1853043cd1b17881bc2ca992
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 16:09:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ bot.on("message", (message) => {
                 message.channel.send({
                     embed: {
                         color: 0x04ff00,
-                        title: miembro.displayName,
+                        title: member.displayName,
                         description: "**Ha verificado a **" + User + "**correctamente**",
                     }
                 });
