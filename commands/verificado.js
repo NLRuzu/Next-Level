78d193b861c1e691f1f39044ddeff734ad244b7c
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 15:02:19 +0200

    Update verificado.js

--- commands/verificado.js
+++ commands/verificado.js
@@ -1,8 +1,11 @@
 const Discord = require("discord.js"); module.exports.run = async (bot, message, args) => {     
 
-let verificado1 = new Discord.RichEmbed     message.mentions.members.first().addRole("✅Verificado");
+let verificado1 = new Discord.RichEmbed     
+message.mentions.members.first().addRole("✅Verificado");
 
-  let verificado1 = new Discord.RichEmbed()     .setTitle(message.mentions.members.first().nickname)     .addField("Verificado", `verificado ${message.mentions.users.first().tag}`)     .setColor("#8904B1"); 
+let verificado1 = new Discord.RichEmbed()     
+.setTitle(message.mentions.members.first().nickname)     
+.addField("Verificado", `verificado ${message.mentions.users.first().tag}`)     .setColor("#8904B1"); 
 
 let partidaschannel = message.guild.channels.find(`name`, "✅-verificados"); 
 if(!partidaschannel) return message.channel.send("Introduce bien el comando");
