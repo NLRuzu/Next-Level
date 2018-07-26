9a5ec8aba51db20abe574bf09dc82594769e8876
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-26 14:56:23 +0200

    Update verificado.js

--- commands/verificado.js
+++ commands/verificado.js
@@ -1,24 +1,16 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"); module.exports.run = async (bot, message, args) => {     
 
-module.exports.run = async (bot, message, args) => {
-      
-	let verificado1 = new Discord.RichEmbed
-	message.mentions.members.first().addRole("✅Verificado")
-	.setTitle(message.member.nickname)
-	.addField("Verificado", `text ${message.mentions.users.first().tag} even more text`)
-    	.setColor("#8904B1");
+let verificado1 = new Discord.RichEmbed     message.mentions.members.first().addRole("✅Verificado");
 
-      let partidaschannel = message.guild.channels.find(`name`, "✅-verificados");
-      if(!partidaschannel) return message.channel.send("Introduce bien el comando");
+  let verificado1 = new Discord.RichEmbed()     .setTitle(message.mentions.members.first().nickname)     .addField("Verificado", `verificado ${message.mentions.users.first().tag}`)     .setColor("#8904B1"); 
 
+let partidaschannel = message.guild.channels.find(`name`, "✅-verificados"); 
+if(!partidaschannel) return message.channel.send("Introduce bien el comando");
 
-    	message.delete().catch(O_o=>{});
-	message.channel.send(verificado1);
+     message.delete().catch(O_o=>{}); 
 
+   partidaschannel.send(verificado1); 
+} 
 
-}
-
-module.exports.help = {
-  name: "verificado"
-}
+module.exports.help = { name: "verificado" }
 
