178a661747d4255152602c01a58dbb1a109de5b3
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-27 10:34:51 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
-    .setTitle("**GENERALES**")
+    .setDescription("**GENERALES**")
     .setColor("#FE2E2E")
     .setThumbnail(sicon)
     .addField("**+serverinfo**", "información del servidor")
