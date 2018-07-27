9f4b942729b4fb4112d207cf44d258b7ff3050ac
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-27 19:28:49 +0200

    Update busco.js

--- commands/busco.js
+++ commands/busco.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) => {
       let juego = args[1];
       let sicon = message.guild.iconURL;
       let reportEmbed = new Discord.RichEmbed()
-      .addField("Usuario:", `${message.author}`)
-      .addField("Busca **"+ cantidad+"** personas para jugar "+juego)
+      .setTitle("Usuario:", `${message.author}`)
+      .setDescription("Busca **"+ cantidad+"** personas para jugar "+juego)
       .setColor("#8904B1")
       .addField("**En la sala**", message.member.voiceChannel);
 
