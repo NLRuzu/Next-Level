2150e8c3f3fb1a0131679e7272a26e688c2225ce
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-30 01:17:25 +0200

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("formato incorrecto +report @usuario razón");
-    User.send("¡Enhorabuena! has sido verificado, ahora puedes ver todo el contenido del servidor del clan. Para más información accede al canal de texto #info");
     let rreason = args.join(" ").slice(22);
     
     
@@ -17,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
 
     let reportschannel = message.guild.channels.find(`name`, "❗-toques");
     if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
-
+User.send("¡Enhorabuena! has sido verificado, ahora puedes ver todo el contenido del servidor del clan. Para más información accede al canal de texto #info");
 
     message.delete().catch(O_o=>{});
     reportschannel.send(reportEmbed);
