373f27edcf103393d94753e10fe9f316665f4a0f
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-07-30 01:31:37 +0200

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -16,7 +16,12 @@ module.exports.run = async (bot, message, args) => {
 
     let reportschannel = message.guild.channels.find(`name`, "❗-toques");
     if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
-    User.send("¡Enhorabuena! has sido verificado, ahora puedes ver todo el contenido del servidor del clan. Para más información accede al canal de texto #info");
+    
+    message.mentions.users.map(async user => {
+        const member = message.guild.member(user);
+        try { await user.send('User.send('¡Enhorabuena! has sido verificado, ahora puedes ver todo el contenido del servidor del clan. Para más información accede al canal de texto #info'); }
+        catch (err) { console.log('error'); }
+            });
 
     message.delete().catch(O_o=>{});
     reportschannel.send(reportEmbed);
