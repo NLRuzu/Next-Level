de344bc88f82da98f67ff234d1a90e47d5fc8bde
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-12 16:10:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ bot.on("guildMemberAdd", async member => {
   console.log(`${member.id} ha entrado al server `);
 
   let welcomechannel = member.guild.channels.find(`name`, "📑-entrada-y-salida");
-  Member.send({
+  User.send({
                embed: {
                         color: 0x04ff00,
                         title: "MENSAJE DE BIENVENIDA",
