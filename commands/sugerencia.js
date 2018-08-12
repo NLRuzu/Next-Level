2d7a241138fd802757cb59d8d4a8b91151fe1e54
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-12 20:07:24 +0200

    Update sugerencia.js

--- commands/sugerencia.js
+++ commands/sugerencia.js
@@ -4,13 +4,6 @@ module.exports.run = async (bot, message, args) => {
     
       let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
       if(!rUser) return message.channel.send("formato incorrecto +proponer @usuario razón de la propuesta");
-      user.send({
-               embed: {
-                        color: 0x04ff00,
-                        title: "SUGERENCIA RECIBIDA",
-                        description: "Enhorabuena, tu sugerencia ha sido recibida con éxito. \n\nGracias por tu aporte."
-                                        }
-                });
       let rreason = args.join(" ").slice(22);
       let sicon = message.guild.iconURL;
       let reportEmbed = new Discord.RichEmbed()
@@ -27,6 +20,7 @@ module.exports.run = async (bot, message, args) => {
 
       message.delete().catch(O_o=>{});
       partidaschannel.send(reportEmbed);
+    
 
 
 }
