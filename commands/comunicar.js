af435e706032244445194f7d71cbffb175366060
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-13 21:35:39 +0200

    Create comunicar.js

---
+++ commands/comunicar.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    
+      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      if(!rUser) return message.channel.send("formato incorrecto +comunicar @usuario razón de la propuesta");
+      let rreason = args.join(" ").slice(22);
+      let sicon = message.guild.iconURL;
+      let reportEmbed = new Discord.RichEmbed()
+      
+      .setTitle("[@everyone]")
+      .setTitle("NUEVO COMUNICADO")
+      .setColor("#15f153")   
+      .addField("Comunicado por:", `${rUser}`)       
+      .addField("Desarrollo del comunicado:", rreason);
+
+
+      let partidaschannel = message.guild.channels.find(`name`, "sugerencias");
+      if(!partidaschannel) return message.channel.send("Introduce bien el comando");
+      
+     
+
+      message.delete().catch(O_o=>{});
+      partidaschannel.send(reportEmbed);
+    
+
+
+}
+
+module.exports.help = {
+  name: "comunicar"
+}
+
