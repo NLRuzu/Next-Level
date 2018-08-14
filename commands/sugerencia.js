7041d863127ad3a6a2898140ff26857246feb10c
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-14 19:18:43 +0200

    Update sugerencia.js

--- commands/sugerencia.js
+++ commands/sugerencia.js
@@ -3,18 +3,18 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     
       let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!rUser) return message.channel.send("formato incorrecto +sugerencia @usuario razón de la propuesta");
+      if(!rUser) return message.channel.send("formato incorrecto +sugerencia @usuario razón de la sugerencia");
       let rreason = args.join(" ").slice(22);
       let sicon = message.guild.iconURL;
       let reportEmbed = new Discord.RichEmbed()
       
       .setTitle("NUEVA SUGERENCIA ")
-      .setColor("#15f153")   
+      .setColor("#00ffdc")   
       .addField("Propuesta por:", `${rUser}`)       
       .addField("Desarrollo de la sugerencia:", rreason);
 
 
-      let partidaschannel = message.guild.channels.find(`name`, "sugerencias");
+      let partidaschannel = message.guild.channels.find(`name`, "💌-sugerencias");
       if(!partidaschannel) return message.channel.send("Introduce bien el comando");
       
       message.mentions.users.map(async user => {
@@ -25,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
 
                embed: {
 
-                        color: 0xFF0000,
+                        color: 0x00ffdc,
 
                         title: "SUGERENCIA RECIBIDA",
 
