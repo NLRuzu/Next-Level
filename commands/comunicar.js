2896637f39bbb35e06fd3ee471fda5ed56c0a3a3
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-14 19:15:15 +0200

    Update comunicar.js

--- commands/comunicar.js
+++ commands/comunicar.js
@@ -3,18 +3,18 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     
       let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!rUser) return message.channel.send("formato incorrecto +comunicar @usuario razón de la propuesta");
+      if(!rUser) return message.channel.send("formato incorrecto +comunicar @miusuario desarrollo");
       let rreason = args.join(" ").slice(22);
       let sicon = message.guild.iconURL;
       let reportEmbed = new Discord.RichEmbed()
       
       .setTitle("NUEVO COMUNICADO")
-      .setColor("#ffe800")    
+      .setColor("#ff0000")    
       .addField("Comunicado por:", `${rUser}`)       
       .addField("Desarrollo del comunicado:", rreason);
 
 
-      let partidaschannel = message.guild.channels.find(`name`, "comunicados");
+      let partidaschannel = message.guild.channels.find(`name`, "📋-comunicados");
       if(!partidaschannel) return message.channel.send("Introduce bien el comando");
       
      
