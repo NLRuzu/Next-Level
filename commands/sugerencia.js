f6c0665f783859d407dbb97a186287251baa681c
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-17 01:36:45 +0200

    Update sugerencia.js

--- commands/sugerencia.js
+++ commands/sugerencia.js
@@ -2,28 +2,32 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-	var comunicado = message.content.replace("+sugerencia ", "");  
-	  var embebido = {
-							"embed": {
+    var comunicado = message.content.replace("+sugerencia ", "");
+      var embebido = {
+                            "embed": {
                                 color: 0x84ff80 ,
                                 author: {
                                     name: message.author.tag,
                                     icon_url: message.author.avatarURL
                                 },
-                                title: "**NUEVA SUGERENCIA**",
-				url: "http://gamedev.es/",				
-								
-				description: comunicado,
-				timestamp: message.createdAt,								
+                                title: "NUEVA SUGERENCIA",
+                url: "http://gamedev.es/",
+
+                description: comunicado,
+                timestamp: message.createdAt,
                             }
-					};
+                    };
 
-				message.channel.send(embebido);
-				message.delete().catch(O_o=>{});
+                message.channel.send(embebido);
+                message.delete().catch(O_o=>{});
 
-		}	
+                if (message.channel.id == "475267748868390912" && message.author.bot) {
+                    message.react("472146792339734565");
+                    message.react("472147160423727105");
+                }
+
+        }
 
 module.exports.help = {
   name: "sugerencia"
 }
-
