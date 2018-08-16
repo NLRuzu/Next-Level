6e7360c83dca98fccb9a858d7eaac9f6f48fbf90
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-16 23:30:41 +0200

    Update comunicar.js

--- commands/comunicar.js
+++ commands/comunicar.js
@@ -1,41 +1,28 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-      let server = bot.guilds.get("458220475957379074");
-      let adminRoleObject = server.roles.find("name", "✅Verificado");
-      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!rUser) return message.channel.send("formato incorrecto +comunicar @miusuario desarrollo");
-      let rreason = args.join(" ").slice(22);
-      let sicon = message.guild.iconURL;
-     
-     message.channel.send({embed: {
-    color: ff0000,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "NUEVO COMUNICADO",
-    
-    fields: [{
-        name: "Desarrollo del comunicado",
-        value: "rreason"
-      },
-              ],
-});
 
-      let partidaschannel = message.guild.channels.find(`name`, "📋-comunicados");
-      if(!partidaschannel) return message.channel.send("Introduce bien el comando");
-      
-     
+	var comunicado = message.content.replace("&comunicar ", "");  
 
-      message.delete().catch(O_o=>{});
-      partidaschannel.send(`[${adminRoleObject}]`, reportEmbed);
-    
+	let adminRoleObject = message.guild.roles.find("name", "Test");
+	  var embebido = {
+							"embed": {
+                                color: 0xc500ff,
+                                author: {
+                                    name: message.author.tag,
+                                    icon_url: message.author.avatarURL
+                                },
+                                title: "NUEVO COMUNICADO",
+								description: comunicado,
+								timestamp: message.createdAt,								
+                            }
+					};
 
+				message.channel.send(`[${adminRoleObject}]`, embebido);
+				message.delete().catch(O_o=>{});
 
-}
+		}	
 
 module.exports.help = {
   name: "comunicar"
 }
-
