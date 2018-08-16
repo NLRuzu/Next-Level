90c4d25fb43d97a37f81652c04b29b66ea77cac6
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-17 00:19:53 +0200

    Update sugerencia.js

--- commands/sugerencia.js
+++ commands/sugerencia.js
@@ -1,50 +1,27 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    
-      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!rUser) return message.channel.send("formato incorrecto +sugerencia @usuario razón de la sugerencia");
-      let rreason = args.join(" ").slice(22);
-      let sicon = message.guild.iconURL;   
-      let sugerenciaEmbed = new Discord.RichEmbed()
-      
-      .setTitle("NUEVA SUGERENCIA ")
-      .setColor("#00ffdc")   
-      .addField("Propuesta por:", `${rUser}`)       
-      .addField("Desarrollo de la sugerencia:", rreason);
 
-		
-      let partidaschannel = message.guild.channels.find(`name`, "💌-sugerencias");
-      if(!partidaschannel) return message.channel.send("Introduce bien el comando");
-      
-      message.mentions.users.map(async user => {
+	var comunicado = message.content.replace("+sugerencia ", "");  
+	  var embebido = {
+							"embed": {
+                                color: 0xff0000,
+                                author: {
+                                    name: message.author.tag,
+                                    icon_url: message.author.avatarURL
+                                },
+                                title: "**NUEVA SUGERENCIA**",
+				url: "http://gamedev.es/",				
+								
+				description: comunicado,
+				timestamp: message.createdAt,								
+                            }
+					};
 
-        const member = message.guild.member(user);
+				message.channel.send(embebido);
+				message.delete().catch(O_o=>{});
 
-        try { await user.send({
-
-               embed: {
-
-                        color: 0x00ffdc,
-
-                        title: "SUGERENCIA RECIBIDA",
-
-                        description: "Tu Sugerencia ha sido enviada correctamente, gracias por aportar tu granito de arena.\n\nUn saludo, el STAFF",
-                    }
-
-                });
-
-            }
-
-        catch (err) { console.log('error'); }
-
-            });
-
-      message.delete().catch(O_o=>{});
-      partidaschannel.send(sugerenciaEmbed);
-      
-
-}
+		}	
 
 module.exports.help = {
   name: "sugerencia"
