b0afb9457993e2c31eb59f2f640511a2af2aeae7
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-17 23:24:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -411,7 +411,27 @@ if(message.content.toUpperCase().startsWith("+STAFF")){
   return message.channel.send(serverembed);
   }
 	
-	
+//+SUGERENCIA
+if(message.content.toUpperCase().startsWith("+SUGERENCIA")){
+  var comunicado = message.content.replace("+sugerencia ", "");
+    var embebido = {
+                          "embed": {
+                              color: 0x84ff80 ,
+                              author: {
+                                  name: message.author.tag,
+                                  icon_url: message.author.avatarURL
+                              },
+                              title: "**NUEVA SUGERENCIA**",
+              url: "http://gamedev.es/",
+
+              description: comunicado,
+              timestamp: message.createdAt,
+                          }
+                  };
+
+              message.channel.send(embebido);
+              message.delete().catch(O_o=>{});
+      }	
 	
 	
 	
