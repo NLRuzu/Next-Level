70e23c80782579bc1c739dc367f9565c70a451d6
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-17 23:38:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -319,32 +319,6 @@ if(message.content.toUpperCase().startsWith("+PARTIDAS")){
    message.channel.send(partidas);
   }	
 
-//+REPORT
-if(message.content.toUpperCase().startsWith("+REPORT")){
-  var report = {
-  "embed": {
-    color: 0xff0000 ,
-    title: "**COMANDOS DE FUNCIONAMIENTO DE TOQUES**",
-    url: "http://gamedev.es/",				
-    fields: [
-      {
-      name: "**+report1 @usuario razón**",
-      value: "Damos el primer toque de atención"
-      },
-      {
-      name: "**+report2 @usuario razón**",
-      value: "Damos el segundo toque de atención"
-      },
-      {
-      name: "**+report3 @usuario razón**",
-      value: "Damos el tercer toque de atención"
-      }
-    ]
-  }
-  };
-  message.delete().catch(O_o=>{});
-  message.channel.send(report);
-  }		
 // +ROLES
 if(message.content.toUpperCase().startsWith("+ROLES")){
   let sicon = message.guild.iconURL;
