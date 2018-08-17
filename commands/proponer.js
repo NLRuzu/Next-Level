6146745f4f1e38b069e1702e65262f6a412a8ffa
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-17 23:45:03 +0200

    Update proponer.js

--- commands/proponer.js
+++ commands/proponer.js
@@ -33,7 +33,8 @@ module.exports.run = async (bot, message, args) => {
 				
                             }
 					};
-
+let reportschannel = message.guild.channels.find(`name`, "❓-propuestas");
+if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
 				message.channel.send(proponer);
 				message.delete().catch(O_o=>{});
 
