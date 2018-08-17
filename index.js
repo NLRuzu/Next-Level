6a8c310c4acd58c84c49079a77ccd7c5e8e63cd3
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-17 22:24:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -252,6 +252,31 @@ if(message.content.toUpperCase().startsWith("+COMUNICAR")){
 
     }	
 
+//+FORTNITE
+if(message.content.toUpperCase().startsWith("+COMUNICAR")){
+  let sicon = message.guild.iconURL;
+  let serverembed = new Discord.RichEmbed()
+  .setDescription("**FORTNITE**")
+  .setColor("#FE2E2E")
+  .setThumbnail(sicon)
+  .addField("**.ign plataforma nickname**", "Vincula tu cuenta de epicgames.")
+  .addField("**.wins**", "Asigna las wins a tu nick, hay que actualizar constantemente.")
+  .addField("**.kd**", "Asigna tu kd a tu nick, hay que actualizar constantemente.")
+  .addField("**.stats**", "Te muestra tus stats generales.")
+  .addField("**.season5**", "Te muestra tus stats de la temporada 5.");
+
+  message.delete().catch(O_o=>{});
+  return message.channel.send(serverembed);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 // CONFIG BOT 
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
