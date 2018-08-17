4b71a2591e14b0c55378aed207f413c538633be1
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-17 23:06:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -319,44 +319,32 @@ if(message.content.toUpperCase().startsWith("+PARTIDAS")){
    message.channel.send(partidas);
   }	
 
-//+PROPONER
-if(message.content.toUpperCase().startsWith("+PROPONER")){
-  let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!rUser) return message.channel.send("formato incorrecto +proponer @usuario razón de la propuesta");
-  let rreason = args.join(" ").slice(22);
-
-  var proponer = {
-                "embed": {
-                                  color: 0xffef52 ,
-                                  author: {
-                                      name: message.author.tag,
-                                      icon_url: message.author.avatarURL
-                                  },
-                                  title: "**NUEVA PROPUESTA**",
-                  url: "http://gamedev.es/",				
-                  
-              fields: [{
-                  name: "Usuario propuesto",
-                  value: `${rUser}`,
-              },
-              {
-                  name: "Fecha:",
-                  value: message.createdAt,
-              },
-              {
-                  name: "Razón de la propuesta:",
-                  value: rreason,
-              }
-              ]
-
-        
-          
-                              }
-            };
-
-    message.channel.send(proponer);
-    message.delete().catch(O_o=>{});
-}		
+//+REPORT
+if(message.content.toUpperCase().startsWith("+REPORT")){
+  var report = {
+  "embed": {
+    color: 0xff0000 ,
+    title: "**COMANDOS DE FUNCIONAMIENTO DE TOQUES**",
+    url: "http://gamedev.es/",				
+    fields: [
+      {
+      name: "**+report1 @usuario razón**",
+      value: "Damos el primer toque de atención"
+      },
+      {
+      name: "**+report2 @usuario razón**",
+      value: "Damos el segundo toque de atención"
+      },
+      {
+      name: "**+report3 @usuario razón**",
+      value: "Damos el tercer toque de atención"
+      }
+    ]
+  }
+  };
+  message.delete().catch(O_o=>{});
+  message.channel.send(report);
+  }		
 	
 // CONFIG BOT 
   if(message.author.bot) return;
