3fbb6e181f0ef6774906cb23e3e2d00ca5f98829
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-08-18 18:56:57 +0200

    Create votar.js

---
+++ commands/votar.js
@@ -0,0 +1,51 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("formato incorrecto +proponer @usuario razón de la propuesta");
+    let rreason = args.join(" ").slice(22);
+    let tipo = args.join(" ").slice(10);
+
+    var proponer = {
+							"embed": {
+                                color: 0xffef52 ,
+                                author: {
+                                    name: message.author.tag,
+                                    icon_url: message.author.avatarURL
+                                },
+                                title: "**NUEVA PROPUESTA**",
+								url: "http://gamedev.es/",				
+								
+						fields: [{
+								name: "Usuario propuesto",
+								value: `${rUser}`,
+						},
+						{
+								name: "Tipo de Sanción:",
+								value: tipo,
+						},
+            {
+								name: "Fecha:",
+								value: message.createdAt,
+						},
+						{
+								name: "Razón de la propuesta:",
+								value: rreason,
+						}
+						]
+
+				
+				
+                            }
+					};
+let reportschannel = message.guild.channels.find(`name`, "❓-propuestas");
+if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+				
+        message.channel.send(proponer);
+				message.delete().catch(O_o=>{});
+
+		}	
+
+module.exports.help = {
+  name: "votar"
+}
