bc3824eb035107e6520cb61cf9aa9005d726cccc
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-09-05 21:40:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,7 @@ fs.readdir("./commands/", (err, files) => {
  
  
   let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
+ 
  
  
 bot.login(token).catch(err => console.log(err));
