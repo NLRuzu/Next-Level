f63297ed94770eede713bc683577c525d71f9c4e
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-09-05 21:35:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const botconfig = require("./botconfig.json");
+const token = process.env.token;
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+ 
+fs.readdir("./commands/", (err, files) => {
+ 
+  if(err) console.log(err);
+ 
+  let jsfile = files.filter(f => f.split(".").pop() == "js")
+  if(jsfile.length <= 0){
+    console.log("No se encuentra el comando");
+    return
+  }
+ 
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} cargado`);
+    bot.commands.set(props.help.name, props);
+  });
+ 
+});
+ 
+ 
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+ 
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+ 
+ 
+});
+ 
+bot.login(token).catch(err => console.log(err));
