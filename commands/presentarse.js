9af35ce61fcf7d9b2eb2a0e8bed3e031f2e9842c
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2018-09-07 14:34:44 +0200

    Update presentarse.js

--- commands/presentarse.js
+++ commands/presentarse.js
@@ -3,40 +3,40 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
       let nick = args[0];
       let edad = args[1];
-	  let ciudad = args[2];
+      let ciudad = args[2];
       let razon = args.slice(3).join(" ");
-    var votar = {
-							"embed": {
+      var presentarse = {
+			"embed": {
                                 color: 0x00d8ff ,
                                 author: {
                                     name: message.author.tag,
                                     icon_url: message.author.avatarURL
-                                },
-                                title: "**NUEVA PRESENTACIÓN DE USUARIO**",
-								url: "http://gamedev.es/",				
+                                		},
+                                		title: "**NUEVA PRESENTACIÓN DE USUARIO**",
+						url: "http://gamedev.es/",				
 								
 						fields: [{
-								name: "Nick de EpicGames",
-								value: nick,
+							name: "Nick de EpicGames",
+							value: nick,
 						},
 						{
-								name: "Edad:",
-								value: edad,
+							name: "Edad:",
+							value: edad,
 						},
 						{
-								name: "Ciudad:",
-								value: ciudad,
+							name: "Ciudad:",
+							value: ciudad,
 						},
-                       {        name: "¿Cómo nos has conocido?:",
-								value: razon,
+                                                {        name: "¿Cómo nos has conocido?:",
+						         value: razon,
 								
 						}
 						]
 
 				
 				
-                            }
-					};
+                                                }
+					        };
 
 				
        let partidaschannel = message.guild.channels.find(`name`, "📜-presentaciones");
@@ -44,7 +44,7 @@ module.exports.run = async (bot, message, args) => {
 
 
       message.delete().catch(O_o=>{});
-      partidaschannel.send(votar);
+      partidaschannel.send(presentarse);
 
 		}	
 
