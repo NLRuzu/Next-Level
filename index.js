42b510769864622f8ea0318e20b32f9704bd7857
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-05-09 21:30:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -991,44 +991,6 @@ if (message.content.startsWith(ft + "musica")) {         //  BOT MUSICAS
 }
 	
 	
-	///// KICK /////
-	
-	
-	  if(message.content.startsWith("+kick")) {
-
-    //!kick @daeshan askin for it
-
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-
-    let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Tiime", message.createdAt)
-    .addField("Reason", kReason);
-	
-	let kickEmbed2 = new Discord.RichEmbed()
-    .setDescription("~Kick LOG~")
-    .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Tiime", message.createdAt)
-    .addField("Reason", kReason);
-
-    let kickChannel = message.guild.channels.find(`name`, "incidents");
-    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
-
-    message.guild.member(kUser).kick(kReason);
-    bot.channels.get("570620409406423060").send(kickEmbed);
-    bot.channels.get("576115603778175005").send(kickEmbed2);
-    return;
-  }
-	
-	///// FIN KICK /////
 	
 if (message.channel.id == message.channel.id) { 	// NUEVOS ROLES TEMPORADAS TEMPORALES
 
