5a32c8dcd9056f2ba521b72d73d57b22125c0d40
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-05-09 21:20:07 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("Can't find user!");
+    let rreason = args.join(" ").slice(22);
+ 
 
     var kick = {
 							"embed": {
