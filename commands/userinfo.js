488d0cb581123883a580b2da2eb1d111a155f4c9
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 14:52:21 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,25 +1,58 @@
-const Discord = module.require('discord.js');
+var commando = require('discord.js-commando');
+var discord = require('discord.js');
 
-module.exports.run = async (bot, message, args) => {
+class aboutuser extends commando.Command 
+{
+    constructor(client) {
+        super(client, {
+            name: 'aboutuser',
+            group: 'help',
+            memberName: 'aboutuser',
+            description: 'Lists information about a specific user.',
+            aliases: ['au', 'aboutu', 'auser', 'user'],
+        })
+    }
+async run(message, args){
+    const userMention = message.mentions.users.first() || msg.author;
+    const memberMention = message.mentions.members.first() || msg.member;
 
-    let user = message.mentions.users.first() || message.author;
-    const joinDiscord = moment(user.createdAt).format('llll');
-    const joinServer = moment(user.joinedAt).format('llll');
-    let embed = new Discord.RichEmbed()
-        .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
-        .setDescription(`${user}`)
-        .setColor(`RANDOM`)
-        .setThumbnail(`${user.displayAvatarURL}`)
-        .addField('Joined at:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-        .addField('Status:', user.presence.status, true)
-        .addField('Roles:', user.roles.map(r => `${r}`).join(' | '), true)
-        .setFooter(`ID: ${user.id}`)
-        .setTimestamp();
+    let userinfo = {};
+    userinfo.bot = userMention.bot;
+    userinfo.createdat = userMention.createdAt;
+    userinfo.discrim = userMention.discriminator;
+    userinfo.id = userMention.id;
+    userinfo.mfa = userMention.mfaEnabled;
+    userinfo.pre = userMention.premium;
+    userinfo.presen = userMention.presence;
+    userinfo.tag = userMention.tag;
+    userinfo.uname = userMention.username;
+    userinfo.verified = userMention.verified;
 
-    message.channel.send({ embed: embed });
-    return;
+    userinfo.avatar = userMention.avatarURL;
+
+    const rolesOfTheMember = memberMention.roles.filter(r => r.name !== '@everyone').map(role => role.name).join(', ')
+
+    var myInfo = new discord.RichEmbed()
+        .setAuthor(userinfo.uname, userinfo.avatar)
+        .addField("Bot?",userinfo.bot, true)
+        .addField("Created At",userinfo.createdat, true)
+        .addField("Discriminator",userinfo.discrim, true)
+        .addField("Client ID",userinfo.id, true)
+        .addField("2FA/MFA Enabled?",userinfo.mfa, true)
+        .addField("Paid Account?",userinfo.pre, true)
+        .addField("Presence",userinfo.presen, true)
+        .addField("Client Tag",userinfo.tag, true)
+        .addField("Username",userinfo.uname, true)
+        .addField("Verified?",userinfo.verified, true)
+        .setColor(0xf0e5da)
+        .setFooter('s!aboutserver')
+        .setTitle("About this user...")
+        .setThumbnail(userinfo.avatar)
+
+
+        message.channel.sendEmbed(myInfo);
+
 }
 
-module.exports.help = {
-    name: 'userinfo'
 }
+module.exports = aboutuser;
