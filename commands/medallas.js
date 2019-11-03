7f9c3ec24096fc82265b9cb3ae52b859d8659470
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 17:02:22 +0100

    Create medallas.js

---
+++ commands/medallas.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  let guildMember;
+
+  if (message.mentions.members.first()) {
+    guildMember = message.mentions.members.first();
+  } else {
+    guildMember = message.member;
+  }
+
+  // We need the User object aswell for different properties
+  const user = guildMember.user;
+  var medallas = [633653927715274772, 633653848434671616];
+  var existe = medallas.includes(idRol);
+  
+  let embed = new Discord.RichEmbed()
+    .setAuthor(user.username)
+    .setDescription("Información")
+    .setColor("#64FF00")
+    .addField("Roles", medallas);
+
+  message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: "medallas"
+}
