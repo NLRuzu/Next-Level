89c2d238ee8f7674b55d403d87ba0ed0a6a42e62
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 19:12:15 +0100

    Update medallas.js

--- commands/medallas.js
+++ commands/medallas.js
@@ -33,7 +33,7 @@ module.exports.run = async (bot, message, args) => {
                             }
                         ]
                     }
-      });
+      })
 
 module.exports.help = {
   name: "medallas"
