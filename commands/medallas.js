01c26010ead8cef916028c15df0fbee1675f54b4
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 20:39:48 +0100

    Update medallas.js

--- commands/medallas.js
+++ commands/medallas.js
@@ -31,35 +31,31 @@ var eventos = ["633025474808774697", "638002315348934658"];
 }
   
   message.channel.send({
-                    embed:{
-                                color: 0xc6ff00,
-                        title: "Información Perfil",
-            description: "Toda la información referente al perfil del usuario",
-            thumbnail: {
-            url: "https://i.imgur.com/v2Sm3d6.png",
-            };
+                    "embed":{
+			    
+            "color": 0xc6ff00,
+            "title": "Información Perfil",
+            "description": "Toda la información referente al perfil del usuario",
+            "thumbnail": {
+            	"url": "https://i.imgur.com/v2Sm3d6.png",
+           		 };
             
-                        fields: 
-                          [
-			{
-			name: "Perfil de",
-			value: `${guildMember}`
-			}
-
-                         {
-                         name:"Eventos Participados",
-                         value: `${strEventos} `   
-                          }
-                          {
-                          name:"Medallas Obtenidas",
-                              value: `${strMedallas} `   
-                          }
-                          
-                          
-                          ]
-                          }
-                          })
-                          }
+            "fields": [
+		{
+		"name": "Perfil de",
+		"value": `${guildMember}`
+		},
+		{
+                 "name":"Eventos Participados",
+                 "value": `${strEventos} `   
+                },
+                {
+                "name":"Medallas Obtenidas",
+                "value": `${strMedallas} `   
+                 }]
+              }
+           })
+       }
 
 module.exports.help = {
   name: "medallas"
