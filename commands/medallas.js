a2d6233b19d23d4e7606203d03bc4fbf9aaa1e86
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 20:28:08 +0100

    Update medallas.js

--- commands/medallas.js
+++ commands/medallas.js
@@ -43,15 +43,15 @@ var eventos = ["633025474808774697", "638002315348934658"];
                           [
 				{
 			name: "Perfil de",
-			value: `${guildMember}`,
+			value: `${guildMember}`
 			},
 				  
 						  {
-                              name:"Eventos Participados",
-                              value: `${strEventos}`   
+                         name:"Eventos Participados",
+                         value: `${strEventos}`   
                           },
 						  {
-                              name:"Medallas Obtenidas",
+                          name:"Medallas Obtenidas",
                               value: `${strMedallas}`   
                           },
 						  
