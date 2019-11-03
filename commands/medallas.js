592eb471e50463db414e34623ca3da36698b68c9
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 20:17:47 +0100

    Update medallas.js

--- commands/medallas.js
+++ commands/medallas.js
@@ -33,7 +33,7 @@ var eventos = ["633025474808774697", "638002315348934658"];
   message.channel.send({
                     embed:{
 		                        color: 0xc6ff00,
-                        description: "Información Perfil de `${guildMember}`",
+                        description: "Información Perfil",
 			
                         fields: 
                           [
