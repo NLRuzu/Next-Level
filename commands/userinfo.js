3ea1c2f2a55f332846ec2bb92df03e10390b03ba
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 15:04:42 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,10 +1,38 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    
-					};
+   let member;
+if (message.mentions.users > 0) {
+    member = message.mentions.user.size()
+} else {
+    member = message.author
+}
+let user;
+if (message.mentions.users > 0) {
+    user = message.mentions.user.size()
+} else {
+    user = message.author
+}
+	
+const user = message.mentions.users.first() || message.author;
+const member = message.mentions.members.first() || message.member;
+if(!member) return message.channel.send('This command can only be run in a guild!')	
+	
+let embed = new Discord.RichEmbed()
+    .setAuthor(user.tag)
+    .setDescription("Users Info", true)
+    .setColor("#64FF00", true)
+    .addField("Full Username:", user.tag , true)
+    .addField("ID:", user.id, true)
+    .addField("Created at:", user.createdAt, true)
+    .addField("Status:", user.presence.status , true)
+    .addField("Game:", user.presence.game ? user.presence.game : 'none' , true)
+    .addField("Roles", member.roles.map(r => `${r}`).join(' | '), true);
+ message.channel.send(embed);			
 
-bot.channels.get("561601089460371457").send(`[${adminRoleObject}]`, ascenso);
+
+
+};
 
 
       message.delete().catch(O_o=>{});
