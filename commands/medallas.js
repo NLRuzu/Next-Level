0ab7750c6d9be941a787abc5df6f2b232851d49c
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 23:21:29 +0100

    Update medallas.js

--- commands/medallas.js
+++ commands/medallas.js
@@ -29,7 +29,15 @@ var eventos = ["633025474808774697", "638002315348934658"];
         strEventos = strEventos + message.guild.roles.get(eventos[i]) + " \n";
     }
 }
-  
+ 
+	if (!strMedallas) {
+strMedallas = "Ninguna";
+}
+	
+if (!strEventos) {
+strEventos = "Ninguna";
+}
+	
   message.channel.send({
                     "embed":{
 			    
