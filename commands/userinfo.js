76edb69f0e481ffc1eb88bf386d8dba0cae8fbfe
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 15:02:39 +0100

    Update and rename aboutuser.js to userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    
+					};
+
+bot.channels.get("561601089460371457").send(`[${adminRoleObject}]`, ascenso);
+
+
+      message.delete().catch(O_o=>{});
+		}	
+
+module.exports.help = {
+  name: "userinfo"
+}
