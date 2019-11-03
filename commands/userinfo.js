2fbd45f60917f8f1d27834822294ac47009615cf
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2019-11-03 14:48:46 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,22 +1,26 @@
-let user;
-if (message.mentions.users.first()) {
-    user = message.mentions.users.first();
-} 
+const Discord = module.require('discord.js');
+const moment = require('moment');
 
-const member = message.guild.member(user);
+module.exports.run = async (bot, message, args) => {
 
-const embed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setThumbnail(message.author.avatarURL)
-    .addField(`${user.tag}`, `${user}`, true)
-    .addField("ID:", `${user.id}`, true)
-    .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
-    .addField("Status:", `${user.presence.status}`, true)
-    .addField("In Server", message.guild.name, true)
-    .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
-    .addField("Bot:", `${user.bot}`, true)
-    .addField("Joined The Server On:", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
-    .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-    .addField("Roles:", member.roles.map(roles => `${roles}`).join(', '), true)
-    .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
-message.channel.send({embed});
+    let user = message.mentions.users.first() || message.author;
+    const joinDiscord = moment(user.createdAt).format('llll');
+    const joinServer = moment(user.joinedAt).format('llll');
+    let embed = new Discord.RichEmbed()
+        .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
+        .setDescription(`${user}`)
+        .setColor(`RANDOM`)
+        .setThumbnail(`${user.displayAvatarURL}`)
+        .addField('Joined at:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+        .addField('Status:', user.presence.status, true)
+        .addField('Roles:', user.roles.map(r => `${r}`).join(' | '), true)
+        .setFooter(`ID: ${user.id}`)
+        .setTimestamp();
+
+    message.channel.send({ embed: embed });
+    return;
+}
+
+module.exports.help = {
+    name: 'userinfo'
+}
