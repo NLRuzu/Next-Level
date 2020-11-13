029bae4d8bee8eeb62308f66e7bbbf8427f45672
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2020-11-13 21:06:02 +0100

    Update perfil.js

--- commands/perfil.js
+++ commands/perfil.js
@@ -4,9 +4,10 @@ module.exports.run = async (bot, message, args) => {
 
   let guildMember;
 	console.log(guildMember);
-  if (message.mentions.members.first()) {
-    guildMember = message.mentions.members.first();
-  
+  if (message.mentions.users.first()) {
+    guildMember = message.mentions.users.first();
+  } else {
+    guildMember = message.user;
   }
 	
 
