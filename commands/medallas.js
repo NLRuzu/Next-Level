96549d76395a9acac7f1fc942600d7ef2c6b0ad8
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2020-11-14 10:52:58 +0100

    Update medallas.js

--- commands/medallas.js
+++ commands/medallas.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
   var strComunidad =""; 
     
     for(let i=0; i<comunidad.length; i++){
-    if(message.mentions.has(comunidad[i])){
+    if(guildMember.roles.cache.has(comunidad[i])){
         strComunidad = strComunidad + message.guild.roles.cache.get(comunidad[i]) + " \n";
     }
 }
