12d93bff1afd8fc8070eb506f963880e279b663a
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2020-11-14 10:23:43 +0100

    Update infomedallas.js

--- commands/infomedallas.js
+++ commands/infomedallas.js
@@ -2,26 +2,6 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  let guildMember;
-
-  if (message.mentions.guild.first()) {
-    guildMember = message.mentions.guild.first();
-  } else {
-    guildMember = message.guild;
-  }
-
-  // We need the User object aswell for different properties
-  const user = guildMember.user;
-  var comunidad = ["633653927715274772"];
-  var strComunidad =""; 
-    
-    for(let i=0; i<comunidad.length; i++){
-    if(guildMember.roles.has(comunidad[i])){
-        strComunidad = strComunidad + message.guild.roles.get(comunidad[i]) + " \n";
-    }
-}
-
-
 	
   message.channel.send({
                     "embed":{
