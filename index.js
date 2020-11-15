ac2c6a2fbf4b92eaafbcf073e9713940390a7a7c
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2020-11-15 12:37:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -283,7 +283,7 @@ bot.on("message", (message) => {
             
             if (message.member.roles.cache.some(role => role.name === '🌟 ADMINISTRADOR')) {
                 var comunicado = message.content.replace("+noticiared ", "");
-                let adminRoleObject = message.guild.roles.find("name", "📑 Noticias Red Dead");
+                let adminRoleObject = message.member.roles.cache.some(role => role.name === '✅Verificado'));
                 var embebido = {
                     "embed": {
                         "color": 0xc6ff00,
