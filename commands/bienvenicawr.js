d54718b6cdd54d032fbb3f8c826ca306e4b9fe66
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2020-12-21 14:14:47 +0100

    Create bienvenicawr.js

---
+++ commands/bienvenicawr.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args, guildMember) => {
+
+    message.channel.send({
+                    "embed":{
+			    
+            color: 0xc6ff00,
+            title: "BIENVENIDO A LA SECCIÓN DE WILD RIFT NEXT LEVEL",
+            description: "Escríbenos tu nick#tag para añadirte a <#790176071806484490>",
+            
+		thumbnail: {
+            url: "https://i.gyazo.com/fbc29e84a7a992cb70277a8f4194c2fc.png",
+        },
+            
+            fields:
+	    [
+            {
+            name: "**EN PRIMER LUGAR**",
+            value: "Diríjete a <#790541015797989376> y asígnate todo lo que tengas ahí",  
+            inline:false 	
+            },
+
+            {
+            name: "**EN SEGUNDO LUGAR**",
+            value: "Puedes usar el comando `+cuenta` en la sala <#790561030693584977> para ver tus datos de *Liga que perteneces, Roles que has seleccionado, Eventos participados y medallas obtenidas*",  
+            inline:false 	
+            }
+	    ]
+        }
+        })
+    }
+
+module.exports.help = {
+  name: "bienvenidawr"
+}
