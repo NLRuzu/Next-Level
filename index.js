d8c4622e09cb33f3afd6209e6464976a1a100391
NLRuzu <41476453+NLRuzu@users.noreply.github.com>
2021-09-02 12:28:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -689,7 +689,7 @@ bot.on("message", (message) => {
 
     // // +SUGERENCIA //
      if (message.content.toUpperCase().startsWith("+SUGERENCIA")) {
-         let adminRoleObject = message.guild.roles.find("name", "🌟 STAFF");
+         let adminRoleObject = message.guild.roles.find("name", "🌟 ADMINISTRADOR");
          var comunicado = message.content.replace("+sugerencia ", "");
          var embebido = {
              "embed": {
